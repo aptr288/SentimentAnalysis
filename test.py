1a5f351fff1b44c12981515f4e490abb14abec37
PRUTHVI TEJA ANUMANDLA <aptr288@gmail.com>
2019-04-18 14:07:58 -0500

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-analyser = SentimentIntensityAnalyzer()
-
-
-def sentiment_analyzer_scores(sentence):
-    score = analyser.polarity_scores(sentence)
-    print("{:-<40} {}".format(sentence, str(score)))
-
-sentiment_analyzer_scores("@realDonaldTrump If you had all that money for a wall WITHOUT congressional authorization, why did you shut down the government?")
